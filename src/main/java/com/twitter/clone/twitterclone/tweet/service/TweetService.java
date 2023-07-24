33170831237a5552a65e33cbdd9bec1583872433
siwoo1224 <96398475+siwoo1224@users.noreply.github.com>
2023-07-24 20:07:23 +0900

    style : 임포트 추가 사항

--- src/main/java/com/twitter/clone/twitterclone/tweet/service/TweetService.java
+++ src/main/java/com/twitter/clone/twitterclone/tweet/service/TweetService.java
@@ -16,8 +16,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;

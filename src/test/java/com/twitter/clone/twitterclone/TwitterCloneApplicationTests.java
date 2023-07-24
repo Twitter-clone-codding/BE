88c3765f22db6643b32cce97227f54610dfd5200
Domae-back-end <ty_ty123@naver.com>
2023-07-24 22:40:43 +0900

    test : 에러로 인한 주석 처리

--- src/test/java/com/twitter/clone/twitterclone/TwitterCloneApplicationTests.java
+++ src/test/java/com/twitter/clone/twitterclone/TwitterCloneApplicationTests.java
@@ -14,24 +14,24 @@ import org.springframework.test.annotation.Rollback;
 @Disabled
 class TwitterCloneApplicationTests {
 
-	@Autowired
-	UserRepository userRepository;
-
-	@Autowired
-	PasswordEncoder passwordEncoder;
-	@Test
-	@DisplayName("로그인 유저 테스트")
-	@Rollback(value = false)
-	void test1() {
-		User user = new User();
-		user.setId(1L);
-		user.setEmail("aaa@gmail.com");
-		String pw =  passwordEncoder.encode("12345678");
-		user.setPassword(pw);
-
-		userRepository.save(user);
-		System.out.println("user.getEmail() = " + user.getEmail());
-		System.out.println("user.getPassword() = " + user.getPassword());
-	}
+//	@Autowired
+//	UserRepository userRepository;
+//
+//	@Autowired
+//	PasswordEncoder passwordEncoder;
+//	@Test
+//	@DisplayName("로그인 유저 테스트")
+//	@Rollback(value = false)
+//	void test1() {
+//		User user = new User();
+//		user.setId(1L);
+//		user.setEmail("aaa@gmail.com");
+//		String pw =  passwordEncoder.encode("12345678");
+//		user.setPassword(pw);
+//
+//		userRepository.save(user);
+//		System.out.println("user.getEmail() = " + user.getEmail());
+//		System.out.println("user.getPassword() = " + user.getPassword());
+//	}
 
 }

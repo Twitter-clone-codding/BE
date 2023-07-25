b0074348774768b379aa8a34c345c21473287b09
siwoo1224 <96398475+siwoo1224@users.noreply.github.com>
2023-07-25 14:23:24 +0900

    refactor : 쿠키저장 세임사이트 설정, cors에러 수정

--- src/main/java/com/twitter/clone/twitterclone/global/config/WebMvcConfig.java
+++ src/main/java/com/twitter/clone/twitterclone/global/config/WebMvcConfig.java
@@ -1,18 +1,28 @@
 package com.twitter.clone.twitterclone.global.config;
 
+import com.sun.mail.imap.protocol.UIDSet;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    private static final List<String> ALLOWED_ORIGINS = Arrays.asList(
+            "http://localhost:3000",
+            "http://172.20.10.10:3000");
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
+        registry.addMapping("/api/**")
+                .allowedOrigins(ALLOWED_ORIGINS.toArray(new String[0])) // 도메인
+//                .allowedOrigins(ALLOWED_ORIGINS[0]) // 도메인
                 .allowedMethods("*")
-                .allowedHeaders("*");
+                .allowCredentials(true)
+                .maxAge(3000);
     }
 
 }

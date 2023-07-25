dd151e54a71cdbbd350b6ad325286a4a7bedb0a7
Domae-back-end <ty_ty123@naver.com>
2023-07-26 00:10:50 +0900

    fix : SuccessKey 예외 처리 제외하고 인증 기능 추가

--- src/main/java/com/twitter/clone/twitterclone/auth/authlogin/service/RegisterService.java
+++ src/main/java/com/twitter/clone/twitterclone/auth/authlogin/service/RegisterService.java
@@ -25,31 +25,35 @@ public class RegisterService {
         String nickname = request.getNickname();
         String birthday = request.getBirthday();
 
-        // 회원 중복 확인
-        Optional<User> checkEmail = userRepository.findByEmail(email);
-        if (checkEmail.isPresent()) {
-            throw new IllegalArgumentException("중복된 사용자가 존재합니다.");
-        }
+        if(request.getSuccessKey().equals(redisUtil.getString("email : "+request.getEmail()))){
 
-        String[] tag = request.getEmail().split("@");
-        String tagName = tag[0];
-        Optional<User> checkTagName = userRepository.findByTagName(tagName);
-        if (checkTagName.isPresent()) {
-            int suffix = 1;
-            while (checkTagName.isPresent()) {
-                tagName = tag[0] + suffix;
-                checkTagName = userRepository.findByTagName(tagName);
-                suffix++;
+            // 회원 중복 확인
+            Optional<User> checkEmail = userRepository.findByEmail(email);
+            if (checkEmail.isPresent()) {
+                throw new IllegalArgumentException("중복된 사용자가 존재합니다.");
             }
+
+            String[] tag = request.getEmail().split("@");
+            String tagName = tag[0];
+            Optional<User> checkTagName = userRepository.findByTagName(tagName);
+            if (checkTagName.isPresent()) {
+                int suffix = 1;
+                while (checkTagName.isPresent()) {
+                    tagName = tag[0] + suffix;
+                    checkTagName = userRepository.findByTagName(tagName);
+                    suffix++;
+                }
+            }
+
+
+            // 사용자 등록
+            User user = new User(email, password, nickname, birthday, tagName);
+            userRepository.save(user);
+            //redis 에 있는 인증코드 삭제.
+            redisUtil.setString("email : "+request.getEmail(), "", 1, TimeUnit.MILLISECONDS);
         }
-
-
-        // 사용자 등록
-        User user = new User(email, password, nickname, birthday, tagName);
-        userRepository.save(user);
-        //redis 에 있는 인증코드 삭제.
-        redisUtil.setString("email : "+request.getEmail(), "", 1, TimeUnit.MILLISECONDS);
 
     }
+    //TODO: 예외처리 인증코드 안맞는다는 에러.
 
 }

e3d320aca72eae09b99277fb8e54110933a1e850
Domae-back-end <ty_ty123@naver.com>
2023-07-25 18:50:06 +0900

    style : security 주석 삭제

--- src/main/java/com/twitter/clone/twitterclone/auth/common/config/WebSecurityConfig.java
+++ src/main/java/com/twitter/clone/twitterclone/auth/common/config/WebSecurityConfig.java
@@ -94,16 +94,6 @@ public class WebSecurityConfig {
         return http.build();
     }
 
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(Arrays.asList("*"));
-//        configuration.setAllowedMethods(Arrays.asList("*"));
-//        configuration.setAllowedHeaders(Arrays.asList("*"));
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {

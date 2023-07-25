e55a2f256c638da6b060405b309e3739d5f8b1e4
soon91 <vbc1124@gmail.com>
2023-07-26 00:40:20 +0900

    fix : @Valid 어노테이션 추가

--- src/main/java/com/twitter/clone/twitterclone/auth/authlogin/controller/RegisterController.java
+++ src/main/java/com/twitter/clone/twitterclone/auth/authlogin/controller/RegisterController.java
@@ -4,6 +4,7 @@ import com.twitter.clone.twitterclone.auth.authlogin.model.request.RegisterReque
 import com.twitter.clone.twitterclone.auth.authlogin.service.RegisterService;
 import com.twitter.clone.twitterclone.auth.common.model.type.ResponseMessage;
 import com.twitter.clone.twitterclone.global.model.response.CustomResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +17,7 @@ public class RegisterController {
     private final RegisterService registerService;
 
     @PostMapping("/register")
-    public CustomResponse<String> register(@RequestBody RegisterRequest request) {
+    public CustomResponse<String> register(@RequestBody @Valid RegisterRequest request) {
         registerService.register(request);
 
         return CustomResponse.success(ResponseMessage.REGISTER_SUCCESS.getMsg(), null);

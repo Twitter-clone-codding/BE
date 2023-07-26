d03a33d302600c2c880624a3b219f8276bbcf2f6
Domae-back-end <ty_ty123@naver.com>
2023-07-27 04:30:50 +0900

    error : merge error 수정

--- src/main/java/com/twitter/clone/twitterclone/profile/repository/ProfileRepository.java
+++ src/main/java/com/twitter/clone/twitterclone/profile/repository/ProfileRepository.java
@@ -3,6 +3,8 @@ package com.twitter.clone.twitterclone.profile.repository;
 import com.twitter.clone.twitterclone.auth.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProfileRepository extends JpaRepository<User, Long> {
     Optional<User> findBytagName(String tagName);
 

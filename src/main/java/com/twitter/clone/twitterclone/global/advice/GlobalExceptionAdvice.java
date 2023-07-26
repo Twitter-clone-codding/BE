40d7dfa1578a834c4513ae1665fd80031b534a9a
Domae-back-end <ty_ty123@naver.com>
2023-07-26 22:52:29 +0900

    fix : 인증코드를 response 하는 부분 수정

--- src/main/java/com/twitter/clone/twitterclone/global/advice/GlobalExceptionAdvice.java
+++ src/main/java/com/twitter/clone/twitterclone/global/advice/GlobalExceptionAdvice.java
@@ -8,6 +8,7 @@ import com.twitter.clone.twitterclone.global.model.response.CustomResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -28,19 +29,23 @@ public class GlobalExceptionAdvice {
     }
 
     @ExceptionHandler(FollowingExceptionImpl.class)
-    public ResponseEntity<?> followingErrorHandler(FollowingExceptionImpl e){
+    public ResponseEntity<?> followingErrorHandler(FollowingExceptionImpl e) {
         return CustomResponse.error(e);
     }
 
     @ExceptionHandler(RegisterExceptionImpl.class)
-    public ResponseEntity<?> registerErrorHandler(RegisterExceptionImpl e){
+    public ResponseEntity<?> registerErrorHandler(RegisterExceptionImpl e) {
 
         return CustomResponse.error(e);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> vaildationErrorHandler(MethodArgumentNotValidException e){
-        return CustomResponse.vaildationError(e.getMessage());
+    public ResponseEntity<?> vaildationErrorHandler(MethodArgumentNotValidException e) {
+
+        return CustomResponse.vaildationError(e.getFieldErrors().stream()
+                .map(FieldError::getDefaultMessage)
+                .findFirst()
+                .orElse("유효성 검사 실패"));
     }
 
 

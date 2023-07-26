904d6c09b53d77ed26bd4567ad82e03c8999a309
Domae-back-end <ty_ty123@naver.com>
2023-07-26 21:51:02 +0900

    fix : 에러 로그 수정

--- src/main/java/com/twitter/clone/twitterclone/auth/common/config/EmailConfig.java
+++ src/main/java/com/twitter/clone/twitterclone/auth/common/config/EmailConfig.java
@@ -36,7 +36,7 @@ public class EmailConfig {
         properties.setProperty("mail.transport.protocol", "smtp"); // 프로토콜 설정
         properties.setProperty("mail.smtp.auth", "true"); // smtp 인증
         properties.setProperty("mail.smtp.starttls.enable", "true"); // smtp strattles 사용
-        properties.setProperty("mail.debug", "true"); // 디버그 사용
+        properties.setProperty("mail.debug", "false"); // 디버그 사용
         properties.setProperty("mail.smtp.ssl.trust","smtp.naver.com"); // ssl 인증 서버는 smtp.naver.com
         properties.setProperty("mail.smtp.ssl.enable","true"); // ssl 사용
         return properties;

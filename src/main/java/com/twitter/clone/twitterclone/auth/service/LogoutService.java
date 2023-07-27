dbf19147b9c6f773e7e9601844ae1ee59479d23a
siwoo1224 <96398475+siwoo1224@users.noreply.github.com>
2023-07-27 17:20:57 +0900

    feat : logout 개발

--- src/main/java/com/twitter/clone/twitterclone/auth/service/LogoutService.java
+++ src/main/java/com/twitter/clone/twitterclone/auth/service/LogoutService.java
@@ -1,5 +1,6 @@
 package com.twitter.clone.twitterclone.auth.service;
 
+import com.twitter.clone.twitterclone.global.util.JwtUtil;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -9,14 +10,15 @@ import org.springframework.stereotype.Service;
 public class LogoutService {
 
     public void logout(HttpServletRequest request, HttpServletResponse response) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                System.out.println(cookie.getName());
-                cookie.setMaxAge(0);
-                cookie.setPath("/");
-                response.addCookie(cookie);
-            }
-        }
+        // Cookie 삭제
+        Cookie cookie = new Cookie(JwtUtil.AUTHORIZATION_HEADER, null); // Name-Value
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true); // SameSite=None 설정을 사용하려면 Secure도 true로 설정해야함.
+        cookie.setAttribute("SameSite", "None");
+        cookie.setMaxAge(0); // 쿠키 유효시간 설정
+
+        // Response 객체에 Cookie 추가
+        response.addCookie(cookie);
     }
 }

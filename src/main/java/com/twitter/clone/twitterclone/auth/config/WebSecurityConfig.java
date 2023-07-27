452824a9705f57b6a9962872d57a33790449cfb7
Domae-back-end <ty_ty123@naver.com>
2023-07-27 15:45:31 +0900

    fix : 프론트 배포 CORS 설정

--- src/main/java/com/twitter/clone/twitterclone/auth/config/WebSecurityConfig.java
+++ src/main/java/com/twitter/clone/twitterclone/auth/config/WebSecurityConfig.java
@@ -83,7 +83,7 @@ public class WebSecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:3000", "https://localhost:8080","https://project-front-rouge.vercel.app"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "https://localhost:8080","https://project-front-rouge.vercel.app", "https://fe-nu-one.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
